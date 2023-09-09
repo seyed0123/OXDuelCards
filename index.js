1015b591f7aaf717c594ad4eb67ec93a1fcff4dd
seyed0123 <seyed123ali123@gmail.com>
2023-09-09 20:47:21 +0430

    choosing cards

--- index.js
+++ index.js
@@ -243,3 +243,228 @@ function initializeNewGame(){
     updateStatusElement(roundElem,"block",primaryColor,`Round <span class='badge'>${roundNum}</span>`)
 
 }
+function startRound()
+{
+    initializeNewRound()
+    collectCards()
+    flipCards(true)
+    shuffleCards()
+
+}
+function initializeNewRound()
+{
+    roundNum++
+    playGameButtonElem.disabled = true
+
+    gameInProgress = true
+    shufflingInProgress = true
+    cardsRevealed = false
+
+    updateStatusElement(currentGameStatusElem, "block", primaryColor, "Shuffling...")
+
+    updateStatusElement(roundElem, "block", primaryColor, `Round <span class='badge'>${roundNum}</span>`)
+
+}
+
+function collectCards(){
+    transformGridArea(collapsedGridAreaTemplate)
+    addCardsToGridAreaCell(cardCollectionCellClass)
+
+}
+
+function transformGridArea(areas)
+{
+    cardContainerElem.style.gridTemplateAreas = areas
+
+}
+function addCardsToGridAreaCell(cellPositionClassName)
+{
+    const cellPositionElem = document.querySelector(cellPositionClassName)
+
+    cards.forEach((card, index) =>{
+        addChildElement(cellPositionElem, card)
+    })
+
+}
+
+function flipCard(card, flipToBack)
+{
+    const innerCardElem = card.firstChild
+
+    if(flipToBack && !innerCardElem.classList.contains('flip-it'))
+    {
+        innerCardElem.classList.add('flip-it')
+    }
+    else if(innerCardElem.classList.contains('flip-it'))
+    {
+        innerCardElem.classList.remove('flip-it')
+    }
+
+}
+
+function flipCards(flipToBack){
+    cards.forEach((card,index)=>{
+        setTimeout(() => {
+            flipCard(card,flipToBack)
+        },index * 100)
+    })
+}
+
+function cardFlyInEffect()
+{
+    const id = setInterval(flyIn, 5)
+    let cardCount = 0
+
+    let count = 0
+
+    function flyIn()
+    {
+        count++
+        if(cardCount == numCards)
+        {
+            clearInterval(id)
+            playGameButtonElem.style.display = "inline-block"
+        }
+        if(count == 1 || count == 250 || count == 500 || count == 750)
+        {
+            cardCount++
+            let card = document.getElementById(cardCount)
+            card.classList.remove("fly-in")
+        }
+    }
+
+
+
+}
+
+function removeShuffleClasses()
+{
+    cards.forEach((card) =>{
+        card.classList.remove("shuffle-left")
+        card.classList.remove("shuffle-right")
+    })
+}
+function animateShuffle(shuffleCount)
+{
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    let card1 = document.getElementById(random1)
+    let card2 = document.getElementById(random2)
+
+    if (shuffleCount % 4 == 0)
+    {
+        card1.classList.toggle("shuffle-left")
+        card1.style.zIndex = 100
+    }
+    if (shuffleCount % 10 == 0)
+    {
+        card2.classList.toggle("shuffle-right")
+        card2.style.zIndex = 200
+    }
+
+}
+
+function shuffleCards()
+{
+    let shuffleCount = 0
+    const id = setInterval(shuffle, 12)
+
+
+    function shuffle()
+    {
+        randomizeCardPositions()
+
+        animateShuffle(shuffleCount)
+
+        if(shuffleCount == 500)
+        {
+            clearInterval(id)
+            shufflingInProgress = false
+            removeShuffleClasses()
+            dealCards()
+            updateStatusElement(currentGameStatusElem, "block", primaryColor, "Please click the card that you think is the Ace of Spades...")
+
+        }
+        else{
+            shuffleCount++
+        }
+
+    }
+
+}
+function randomizeCardPositions()
+{
+    const random1 = Math.floor(Math.random() * numCards) + 1
+    const random2 = Math.floor(Math.random() * numCards) + 1
+
+    const temp = cardPositions[random1 - 1]
+
+    cardPositions[random1 - 1] = cardPositions[random2 - 1]
+    cardPositions[random2 - 1] = temp
+
+}
+function dealCards()
+{
+    addCardsToAppropriateCell()
+    const areasTemplate = returnGridAreasMappedToCardPos()
+
+    transformGridArea(areasTemplate)
+
+}
+function returnGridAreasMappedToCardPos()
+{
+    let firstPart = ""
+    let secondPart = ""
+    let areas = ""
+
+    cards.forEach((card, index) => {
+        if(cardPositions[index] == 1)
+        {
+            areas = areas + "a "
+        }
+        else if(cardPositions[index] == 2)
+        {
+            areas = areas + "b "
+        }
+        else if (cardPositions[index] == 3)
+        {
+            areas = areas + "c "
+        }
+        else if (cardPositions[index] == 4)
+        {
+            areas = areas + "d "
+        }
+        if (index == 1)
+        {
+            firstPart = areas.substring(0, areas.length - 1)
+            areas = "";
+        }
+        else if (index == 3)
+        {
+            secondPart = areas.substring(0, areas.length - 1)
+        }
+
+    })
+
+    return `"${firstPart}" "${secondPart}"`
+
+
+}
+
+
+function addCardsToAppropriateCell()
+{
+    cards.forEach((card)=>{
+        addCardToGridCell(card)
+    })
+}
+
+
+
+function createCards()
+{
+    cardObjectDefinitions.forEach((cardItem)=>{
+        createCard(cardItem)
+    })
+}
