f3fda5b48e5f910ba8d5b69a567bbc9bb8938eb8
seyed0123 <seyed123ali123@gmail.com>
2023-09-09 20:50:10 +0430

    game almost finished

--- index.js
+++ index.js
@@ -468,3 +468,152 @@ function createCards()
         createCard(cardItem)
     })
 }
+function createCard(cardItem){
+
+    //create div elements that make up a card
+    const cardElem = createElement('div')
+    const cardInnerElem = createElement('div')
+    const cardFrontElem = createElement('div')
+    const cardBackElem = createElement('div')
+
+    //create front and back image elements for a card
+    const cardFrontImg = createElement('img')
+    const cardBackImg = createElement('img')
+
+    //add class and id to card element
+    addClassToElement(cardElem, 'card')
+    addClassToElement(cardElem, 'fly-in')
+    addIdToElement(cardElem, cardItem.id)
+
+    //add class to inner card element
+    addClassToElement(cardInnerElem, 'card-inner')
+
+    //add class to front card element
+    addClassToElement(cardFrontElem, 'card-front')
+
+    //add class to back card element
+    addClassToElement(cardBackElem, 'card-back')
+
+    //add src attribute and appropriate value to img element - back of card
+    addSrcToImageElem(cardBackImg, cardBackImgPath)
+
+    //add src attribute and appropriate value to img element - front of card
+    addSrcToImageElem(cardFrontImg, cardItem.imagePath)
+
+    //assign class to back image element of back of card
+    addClassToElement(cardBackImg, 'card-img')
+
+    //assign class to front image element of front of card
+    addClassToElement(cardFrontImg, 'card-img')
+
+    //add front image element as child element to front card element
+    addChildElement(cardFrontElem, cardFrontImg)
+
+    //add back image element as child element to back card element
+    addChildElement(cardBackElem, cardBackImg)
+
+    //add front card element as child element to inner card element
+    addChildElement(cardInnerElem, cardFrontElem)
+
+    //add back card element as child element to inner card element
+    addChildElement(cardInnerElem, cardBackElem)
+
+    //add inner card element as child element to card element
+    addChildElement(cardElem, cardInnerElem)
+
+    //add card element as child element to appropriate grid cell
+    addCardToGridCell(cardElem)
+
+    initializeCardPositions(cardElem)
+
+    attatchClickEventHandlerToCard(cardElem)
+
+
+}
+function attatchClickEventHandlerToCard(card){
+    card.addEventListener('click', () => chooseCard(card))
+}
+
+function initializeCardPositions(card)
+{
+    cardPositions.push(card.id)
+}
+
+function createElement(elemType){
+    return document.createElement(elemType)
+
+}
+function addClassToElement(elem, className){
+    elem.classList.add(className)
+}
+function addIdToElement(elem, id){
+    elem.id = id
+}
+function addSrcToImageElem(imgElem, src){
+    imgElem.src = src
+}
+function addChildElement(parentElem, childElem){
+    parentElem.appendChild(childElem)
+}
+
+function addCardToGridCell(card)
+{
+    const cardPositionClassName = mapCardIdToGridCell(card)
+
+    const cardPosElem = document.querySelector(cardPositionClassName)
+
+    addChildElement(cardPosElem, card)
+
+}
+function mapCardIdToGridCell(card){
+
+    if(card.id == 1)
+    {
+        return '.card-pos-a'
+    }
+    else if(card.id == 2)
+    {
+        return '.card-pos-b'
+    }
+    else if(card.id == 3)
+    {
+        return '.card-pos-c'
+    }
+    else if(card.id == 4)
+    {
+        return '.card-pos-d'
+    }
+}
+
+//local storage functions
+function getSerializedObjectAsJSON(obj)
+{
+    return JSON.stringify(obj)
+}
+function getObjectFromJSON(json)
+{
+    return JSON.parse(json)
+}
+function updateLocalStorageItem(key, value)
+{
+    localStorage.setItem(key, value)
+}
+function removeLocalStorageItem(key)
+{
+    localStorage.removeItem(key)
+}
+function getLocalStorageItemValue(key)
+{
+    return localStorage.getItem(key)
+}
+
+function updateGameObject(score,round)
+{
+    gameObj.score = score
+    gameObj.round = round
+}
+function saveGameObjectToLocalStorage(score,round)
+{
+    updateGameObject(score, round)
+    updateLocalStorageItem(localStorageGameKey, getSerializedObjectAsJSON(gameObj))
+}
